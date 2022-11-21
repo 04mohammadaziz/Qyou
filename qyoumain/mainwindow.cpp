30b80ef0f85dc8111b339e0139a76af5a1b2f789
Ciaran Byles-Ho <ciaran.bylesho@queensu.ca>
2022-11-21 00:16:40 -0500

    Setup multipage system for home page

--- qyoumain/mainwindow.cpp
+++ qyoumain/mainwindow.cpp
@@ -13,3 +13,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_studentMapButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
--- qyoumain/mainwindow.h
+++ qyoumain/mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_studentMapButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
